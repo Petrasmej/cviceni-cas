c93aa4268002d1508c3a94429f6c564eaeef6086
Petra Smejkalova <smejkalova.petra@gmail.com>
2024-03-04 13:16:16 +0100

    Vyber zony

--- cviceni-cas/src/components/Homepage.jsx
+++ cviceni-cas/src/components/Homepage.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 
+/* ukol Pražský čas a Výběr zony */
 const Homepage = () => {
   const [dateTime, setDateTime] = useState('');
+  const [timezone, setTimezone] = useState('Europe/Prague');
 
   useEffect(() => {
     const fetchDate = async () => {
@@ -11,16 +13,31 @@ const Homepage = () => {
       );
       const data = await response.json();
 
-      setDateTime(data.dateTime.slice(0, 16));
+      setDateTime(data.dateTime);
     };
 
     fetchDate();
-  }, []);
+  }, [timezone]);
+
+  const handleTimezoneChange = (e) => {
+    setTimezone(e.target.value);
+  };
 
   return (
     <div>
       <h1>Vítejte</h1>
       <p>Aktuální datum a čas: {dateTime}</p>
+      <form>
+        <label>Vyberte časovou zonu:</label>
+        <select value={timezone} onChange={handleTimezoneChange}>
+          <option value="America/New_York">New York</option>
+          <option value="Europe/London">Londýn</option>
+          <option value="Europe/Moscow">Moskva</option>
+          <option value="Europe/Prague">Praha</option>
+          <option value="Asia/Hong_Kong">Hong Kong</option>
+          <option value="Asia/Jerusalem">Jeruzalém</option>
+        </select>
+      </form>
     </div>
   );
 };
